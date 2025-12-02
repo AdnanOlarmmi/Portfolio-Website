d170b04a52000f06032db818594f541b3c2cc7d0
Adnan Is-haq Olamilekan <olarmmi@gmail.com>
2025-12-02 09:57:08 +0100

    Change image format from PNG to JPG

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 /*eslint-disable*/
 import React, { useRef } from 'react';
-import adnan from './../assets/img/mainpic.png';
+import adnan from './../assets/img/mainpic.jpg';
 import AnchorLink from "react-anchor-link-smooth-scroll";
 import DarkModeIcon from '@mui/icons-material/DarkMode';
 import LightModeIcon from '@mui/icons-material/LightMode';
